1fcf3a0bd8d4df1f0eaeec72929c5ecba30bb4a1
Tomer Raz <tomer532010@gmail.com>
2022-08-19 00:08:45 +0300

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import TasksPage from "./pages/tasks/TasksPage.component";
 const App = () => {
   return (
     <div>
-      <h1>Vegvizer home assignment</h1>
-      <h2>Updated version of the Todo List</h2><hr />
+      <div style={{ backgroundColor: "#0070c9", color: "white", display: "flex", justifyContent: "center" }}>
+        <h1 >Vegvizer home assignment - Updated version of the Todo List</h1>
+      </div>
       <TasksPage />
     </div>
   );
